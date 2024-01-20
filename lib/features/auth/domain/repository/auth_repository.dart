31887658a927d3f6b71ad3f8bc30980bdf087db4
Sahanshil Thapa <94502748+sunsilthapa@users.noreply.github.com>
2024-01-20 15:35:48 +0545

    login usecase and repository

--- lib/features/auth/domain/repository/auth_repository.dart
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -8,8 +8,9 @@ import 'package:meat_shop/core/failure/failure.dart';
 import 'package:meat_shop/features/auth/data/repository/auth_remote_repository_impl.dart';
 import 'package:meat_shop/features/auth/domain/entity/auth_entity.dart';
 
-final authRepositoryProvider = Provider.autoDispose<AuthRepository>((ref) => ref.read(authRemoteRepositoryProvider));
+final authRepositoryProvider = Provider<AuthRepository>((ref) => ref.read(authRemoteRepositoryProvider));
 
 abstract class AuthRepository {
   Future<Either<Failure, bool>> register(AuthEntity user);
+  Future<Either<Failure, bool>> login(String username, String password);
 }
--- lib/features/auth/domain/usecase/auth_usecase.dart
+++ lib/features/auth/domain/usecase/auth_usecase.dart
@@ -2,22 +2,42 @@
 
 import 'package:dartz/dartz.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meat_shop/config/constants/api_endpoints.dart';
 import 'package:meat_shop/core/failure/failure.dart';
+import 'package:meat_shop/core/shared_prefs/user_shared_prefs.dart';
 import 'package:meat_shop/features/auth/domain/entity/auth_entity.dart';
 import 'package:meat_shop/features/auth/domain/repository/auth_repository.dart';
 
 
-final authUseCaseProvider = Provider.autoDispose<AuthUseCase>((ref) => AuthUseCase(authRepository: ref.read(authRepositoryProvider)));
+final authUseCaseProvider = Provider.autoDispose<AuthUseCase>((ref) => AuthUseCase( ref.read(authRepositoryProvider), ref.read(userSharedPrefsProvider)));
 class AuthUseCase{
-  final AuthRepository authRepository;
+  final AuthRepository _authRepository;
+  final UserSharedPrefs userSharedPrefs;
 
-  AuthUseCase({
-    required this.authRepository
-  });
+
+  AuthUseCase(
+   this._authRepository,
+    this.userSharedPrefs
+  );
 
 
   Future<Either<Failure,bool>> addUser(AuthEntity user) async{
-    return await authRepository.register(user);
+    return await _authRepository.register(user);
     
+  }
+
+   Future<Either<Failure, bool>> login(
+    String username,
+    String password,
+  ) async {
+    //* call the repo to perform login
+    final result =await _authRepository.login(username, password);
+
+    //* if login success,
+    if(result.isRight()){
+      //* save user token to shared preferences
+      await userSharedPrefs.setUserToken(ApiEndpoints.token);
+    }
+    return result;
   }
 }
